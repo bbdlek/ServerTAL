5f9dc31fc1b8f108923e3468cf1c9d218f934782
bbdlek <bbdlek@gmail.com>
2022-08-11 05:49:19 +0900

    초기 커밋.

---
+++ ServerTAL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ServerTAL
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ServerTAL";
+
+            Server.Start(50, 26950);
+            
+            Console.ReadKey();
+        }
+    }
+}
